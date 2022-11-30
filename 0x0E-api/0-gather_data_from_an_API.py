bd37992b83dfced51b95aae95a0a1d3959959339
JamesTeeters <4925@holbertonstudents.com>
2022-11-30 13:07:33 -0800

    made script a function

--- 0x0E-api/0-gather_data_from_an_API.py
+++ 0x0E-api/0-gather_data_from_an_API.py
@@ -3,38 +3,45 @@
 from sys import argv
 import json
 import requests
-"""argv argument is user_id"""
-user_id = argv[1]
 
-"""get todo information"""
-todo_response = requests.get('https://jsonplaceholder.typicode.com/todos/\
-?userId={}'.format(user_id))
 
-"""get user information"""
-user_response = requests.get('https://jsonplaceholder.typicode.com/users/\
-{}'.format(user_id))
+def task0():
+    """argv argument is user_id"""
+    user_id = argv[1]
 
-"""parse through todo information"""
-todo_data = todo_response.text
-parse_todo = json.loads(todo_data)
+    """get todo information"""
+    todo_response = requests.get('https://jsonplaceholder.typicode.com/todos/\
+    ?userId={}'.format(user_id))
 
-"""parse through user information"""
-user_data = user_response.text
-parse_user = json.loads(user_data)
+    """get user information"""
+    user_response = requests.get('https://jsonplaceholder.typicode.com/users/\
+    {}'.format(user_id))
 
-"""task functions"""
-name = parse_user['name']
-completed_tasks = 0
-total_tasks = 0
+    """parse through todo information"""
+    todo_data = todo_response.text
+    parse_todo = json.loads(todo_data)
 
-for task in parse_todo:
-    total_tasks += 1
-    if task['completed']:
-        completed_tasks += 1
+    """parse through user information"""
+    user_data = user_response.text
+    parse_user = json.loads(user_data)
 
-print("Employee {} is done with tasks({}/{}):\
-".format(name, completed_tasks, total_tasks))
+    """task functions"""
+    name = parse_user['name']
+    completed_tasks = 0
+    total_tasks = 0
 
-for task in parse_todo:
-    if task['completed']:
-        print("\t {}\n".format(task['title']), end="")
+    for task in parse_todo:
+        total_tasks += 1
+        if task['completed']:
+            completed_tasks += 1
+
+    print("Employee {} is done with tasks({}/{}):\
+    ".format(name, completed_tasks, total_tasks))
+
+    for task in parse_todo:
+        if task['completed']:
+            print("\t {}\n".format(task['title']), end="")
+
+
+if __name__ == '__main__':
+    task0()
