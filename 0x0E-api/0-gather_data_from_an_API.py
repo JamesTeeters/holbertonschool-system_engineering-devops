39125147816f614adb88c705b92449b7b9489161
JamesTeeters <4925@holbertonstudents.com>
2022-11-30 13:00:20 -0800

    changed import order

--- 0x0E-api/0-gather_data_from_an_API.py
+++ 0x0E-api/0-gather_data_from_an_API.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """Returns Employee information about todo list progression."""
+from sys import argv
 import json
 import requests
-from sys import argv
 """argv argument is user_id"""
 user_id = argv[1]
 
