604f508791ccc10312066a6d9c31d7c73d72eab7
JamesTeeters <4925@holbertonstudents.com>
2022-11-30 19:34:45 -0800

    made files executable

---
+++ 0x0E-api/1-export_to_CSV.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""Returns Employee information about todo list progression."""
+import json
+import requests
+from sys import argv
+
+
+if __name__ == '__main__':
+    """argv argument is user_id"""
+    user_id = argv[1]
+
+    """get todo information"""
+    todo_response = requests.get('https://jsonplaceholder.typicode.com/todos/\
+?userId={}'.format(user_id))
+
+    """get user information"""
+    user_response = requests.get('https://jsonplaceholder.typicode.com/users/\
+{}'.format(user_id))
+
+    """parse through todo information"""
+    todo_data = todo_response.text
+    parse_todo = json.loads(todo_data)
+
+    """parse through user information"""
+    user_data = user_response.text
+    parse_user = json.loads(user_data)
+
+    """task functions"""
+    user_name = parse_user['name']
+    completed_tasks = 0
+    total_tasks = 0
+
+    for task in parse_todo:
+        total_tasks += 1
+        if task['completed']:
+            completed_tasks += 1
+
+    print("Employee {} is done with tasks({}/{}):\
+    ".format(user_name, completed_tasks, total_tasks))
+
+    for task in parse_todo:
+        if task['completed']:
+            print("\t {}\n".format(task['title']), end="")
